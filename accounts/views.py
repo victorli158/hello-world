7ddc04860c516eb4d7176d073a63ee4055823fa3
Miles Hobby <mhobby16@gmail.com>
2017-06-06 19:31:05 -0700

    add password error checking

--- accounts/views.py
+++ accounts/views.py
@@ -5,12 +5,14 @@ from rest_framework.response import Response
 import pdb
 from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
+from django.core.serializers import serialize
+from django.contrib.auth.password_validation import validate_password, MinimumLengthValidator
+
 # Create your views here.
 
 class SignUp(APIView):
 
     def post(self, request, format='json'):
-        pdb.set_trace()
         username = request.data["username"]
         email = request.data["email"]
         password = request.data["password"]
@@ -19,9 +21,14 @@ class SignUp(APIView):
             user.full_clean()
         except ValidationError as e:
             return JsonResponse(e.message_dict)
+        try:
+            validate_password(password, user)
+        except ValidationError as e:
+            return JsonResponse({'errors': e.messages})
         user.set_password(user.password)
         user.save()
-        return JsonResponse(user, safe="False")
+        user = serialize('json', [user])
+        return JsonResponse(user, safe=False)
 
     def get(self, request):
         return HttpResponse("yoo")
