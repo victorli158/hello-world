f16e7b67cbae3517a4acff118a4303f0956220fb
seanperfecto <seanperfecto@gmail.com>
2017-06-07 09:51:24 -0700

    Merge branch

--- planit/settings.py
+++ planit/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'trips.apps.TripsConfig',
 ]
 
 MIDDLEWARE = [
